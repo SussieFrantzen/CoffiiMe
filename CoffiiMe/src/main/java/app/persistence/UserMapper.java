1629307dc2d15a29b6734b74bb1f03f9fd878a1c
SussieFrantzen <cph-sf252@stud.ek.dk>
2026-03-23 18:22:50 +0100

    HTML skellet done + end_points set. UserController is done. Chanching in UserMapper.

--- CoffiiMe/src/main/java/app/persistence/UserMapper.java
+++ CoffiiMe/src/main/java/app/persistence/UserMapper.java
@@ -1,100 +1,80 @@
 package app.persistence;
 
-import app.entities.Users;
 import app.exception.DatabaseException;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+
 public class UserMapper {
 
-    public static Users login(String email, String password, ConnectionPool connectionPool) throws DatabaseException
-    {
+    private ConnectionPool connectionPool;
+    public UserMapper(ConnectionPool connectionPool) {
+        this.connectionPool = connectionPool;
+    }
+
+    public boolean login(String email, String password) {
         String sql = """
-            SELECT u.user_id, u.firstname, u.lastname, u.email, a.password
-            FROM users u
-            JOIN account a ON u.user_id = a.user_id
-            WHERE u.email = ? AND a.password = ?
-            """;
+                SELECT u.user_id, u.firstname, u.lastname, u.email, a.password
+                FROM users u
+                JOIN account a ON u.user_id = a.user_id
+                WHERE u.email = ? AND a.password = ?
+                """;
 
-        try (
-                Connection connection = connectionPool.getConnection();
-                PreparedStatement ps = connection.prepareStatement(sql)
-        )
-        {
-            ps.setString(1, email);
-            ps.setString(2, password);
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
-            ResultSet rs = ps.executeQuery();
+                preparedStatement.setString(1, email);
+                preparedStatement.setString(2, password);
 
-            if (rs.next())
-            {
-                return new Users(
-                        rs.getString("firstname"),
-                        rs.getString("lastname"),
-                        rs.getString("email"),
-                        rs.getString("password"),
-                        rs.getInt("user_id")
-                );
+                ResultSet resultSet = preparedStatement.executeQuery();
+                return resultSet.next();
             }
-            else
-            {
-                throw new DatabaseException("Forkert email eller password");
-            }
-        }
-        catch (SQLException e)
-        {
-            throw new DatabaseException("Login fejl", e.getMessage());
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
         }
     }
 
-    public static void createUser(String firstname, String lastname, String email, String password, ConnectionPool connectionPool) throws DatabaseException
-    {
-        String insertUserSql = "INSERT INTO users (firstname, lastname, email) VALUES (?, ?, ?)";
-        String insertAccountSql = "INSERT INTO account (user_id, password) VALUES (?, ?)";
+    public boolean createUser(String firstname, String lastname, String email, String password) throws DatabaseException {
+        String createUser = "INSERT INTO users (firstname, lastname, email) VALUES (?, ?, ?)";
+        String createAccount = "INSERT INTO account (user_id, password) VALUES (?, ?)";
 
-        try (Connection connection = connectionPool.getConnection())
-        {
+        try (Connection connection = connectionPool.getConnection()) {
             connection.setAutoCommit(false);
 
             int userId;
 
-            // 🔹 Insert i users + hent generated key
-            try (PreparedStatement psUser = connection.prepareStatement(insertUserSql, PreparedStatement.RETURN_GENERATED_KEYS))
-            {
-                psUser.setString(1, firstname);
-                psUser.setString(2, lastname);
-                psUser.setString(3, email);
+            try (PreparedStatement user = connection.prepareStatement(createUser, PreparedStatement.RETURN_GENERATED_KEYS)) {
+                user.setString(1, firstname);
+                user.setString(2, lastname);
+                user.setString(3, email);
 
-                psUser.executeUpdate();
-
-                ResultSet rs = psUser.getGeneratedKeys();
-                if (rs.next())
-                {
+                user.executeUpdate();
+                ResultSet rs = user.getGeneratedKeys();
+                if (rs.next()) {
                     userId = rs.getInt(1);
-                }
-                else
-                {
+                } else {
                     throw new DatabaseException("Kunne ikke hente user_id");
                 }
             }
 
-            // 🔹 Insert i account
-            try (PreparedStatement psAccount = connection.prepareStatement(insertAccountSql))
-            {
-                psAccount.setInt(1, userId);
-                psAccount.setString(2, password);
+            try (PreparedStatement account = connection.prepareStatement(createAccount)) {
+                account.setInt(1, userId);
+                account.setString(2, password);
 
-                psAccount.executeUpdate();
+                account.executeUpdate();
+
             }
-
             connection.commit();
-        }
-        catch (SQLException e)
-        {
-            throw new DatabaseException("Fejl ved oprettelse af bruger", e.getMessage());
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
         }
     }
-    }
+}
 
