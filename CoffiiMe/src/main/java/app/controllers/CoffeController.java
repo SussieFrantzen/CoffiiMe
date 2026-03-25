61c836815bac5525f63acc2a9b508c3cdd60ab11
ChristianKenter <cph-cn445@stud.ek.dk>
2026-03-25 17:28:41 +0100

    Javascript up running in colorpanel. content added in  create method CoffeeMapper class

--- CoffiiMe/src/main/java/app/controllers/CoffeController.java
+++ CoffiiMe/src/main/java/app/controllers/CoffeController.java
@@ -1,18 +1,26 @@
 package app.controllers;
 
+import app.entities.Coffee;
 import app.persistence.CoffeeMapper;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import java.awt.*;
+
 public class CoffeController {
 
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
-        app.post("/index", ctx -> getExistingCoffee(ctx, connectionPool));
-        app.post("/index", ctx -> createCoffee(ctx, connectionPool));
+        //app.post("/index", ctx -> getExistingCoffee(ctx, connectionPool));
+        //app.post("/index", ctx -> createCoffee(ctx, connectionPool));
+        //app.get("/color", ctx -> ctx.json(new Integer[]{255,200,5}));
+
+
+        app.get("/color", ctx -> createCoffee(ctx, connectionPool));
     }
 
 
+    /*
     public static void getExistingCoffee(Context ctx, ConnectionPool connectionPool) {
         String beanType = ctx.formParam("");
         String coffeeType = ctx.formParam("");
@@ -21,16 +29,25 @@ public class CoffeController {
         CoffeeMapper coffeeMapper = new CoffeeMapper(connectionPool);
         coffeeMapper.getExistingCoffee(beanType, totalVolume);
     }
+     */
 
     public static void createCoffee(Context ctx, ConnectionPool connectionPool) {
+
         String beanType = ctx.formParam("");
         int totalVolume = Integer.parseInt(ctx.formParam(""));
         int beanPercentages = Integer.parseInt(ctx.formParam(""));
         int milkPercentages = Integer.parseInt(ctx.formParam(""));
         int waterPercentages = Integer.parseInt(ctx.formParam(""));
 
-        CoffeeMapper coffeMapper = new CoffeeMapper(connectionPool);
-        coffeMapper.createCoffee(beanType, totalVolume, beanPercentages, milkPercentages, waterPercentages);
+        CoffeeMapper coffee = new CoffeeMapper(connectionPool);
+        coffee.createCoffee(beanType, totalVolume, beanPercentages, milkPercentages, waterPercentages);
+
+
+        Color color = coffee.createCoffee(beanType, totalVolume, beanPercentages, milkPercentages, waterPercentages).getColor();
+        String rgb = "rgb(" + color.getRed() + "," + color.getGreen() + "," + color.getBlue() + ")";
+        ctx.result(rgb);
+
+
     }
 
     public static void addToFavorites() {
